6927228859463b0a79feccd5bc3e45f054da870e
Thomas <Thomas60808@gmail.com>
2022-07-07 19:15:09 +0800

    finish gender

--- main.cpp
+++ main.cpp
@@ -4,10 +4,22 @@ using namespace std;
 
 int main(){
     string name = "";
+    char gender = '\0';
     cout << "please enter your name?" << endl;
     cin >> name ;
-    
+
     cout << "hello " << name << endl;
+
+    cout << "please enter your gender?(M/F)" << endl;
+    cin >> gender;
+    if(gender == 'M' || gender == 'm')
+        cout << "Hi Mr. " << name << "." << endl;
+    else if(gender == 'F' || gender == 'f')
+        cout << "Hi Ms. " << name << "." << endl;
+    else
+        cout << "Hi " << name << "." << endl;
+    
+
     system("pause");
     return 0;
 }
