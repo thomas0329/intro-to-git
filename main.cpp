0075452cb9d9db97294bcad835bf0ec04a7f8d22
Thomas <Thomas60808@gmail.com>
2022-07-07 18:40:26 +0800

    finish age

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main(){
     string name = "";
+    unsigned int age = 0;
     cout << "please enter your name?" << endl;
     cin >> name ;
     
     cout << "hello " << name << endl;
+    cout << "how old are you?" << endl;
+    cin >> age;
+    if(!age > 0)
+        age  = 0;
+    cout << "You are " << age << " year(s) old." << endl;
     system("pause");
     return 0;
 }
